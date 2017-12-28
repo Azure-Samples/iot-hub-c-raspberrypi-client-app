3293ea75956bc8b7c849e10f8ce6ac3b9005ca04
Renlong Tu <turenlong@qq.com>
2017-12-28 11:23:56 +0800

    Add hint for user to reset telemetry settings, disable client ip when user selects "no" (#21)
    * Add hint for user to reset telemetry settings, disable client ip when user selects "no"
    
    * Code style change to make cpplint happy

--- telemetry.c
+++ telemetry.c
@@ -12,7 +12,7 @@ static const char *DEVICE = "RaspberryPi";
 static const char *MCU = "STM32F412";
 static const char *MAC_TEMPLATE = "%02X-%02X-%02X-%02X-%02X-%02X";
 static const int HASH_LEN = 65;
-static const char *BODY_TEMPLATE =
+static const char *BODY_TEMPLATE_FULL =
 "{"
     "\"data\": {"
         "\"baseType\": \"EventData\","
@@ -35,6 +35,67 @@ static const char *BODY_TEMPLATE =
     "\"name\": \"%s\","
     "\"iKey\": \"%s\""
 "}";
+
+static const char *BODY_TEMPLATE_MIN =
+"{"
+    "\"data\": {"
+        "\"baseType\": \"EventData\","
+        "\"baseData\": {"
+            "\"properties\": {"
+                "\"language\": \"C\","
+                "\"device\": \"RaspberryPi\""
+            "},"
+            "\"name\": \"%s\""
+        "}"
+    "},"
+    "\"time\": \"%s\","
+    "\"name\": \"%s\","
+    "\"iKey\": \"%s\","
+    "\"tags\": {"
+        "\"ai.location.ip\": \"0.0.0.0\""
+    "}"
+"}";
+
+static const char *PROMPT_TEXT = "\nMicrosoft would like to collect data about how users use Azure IoT " \
+"samples and some problems they encounter. Microsoft uses this information to improve "\
+"our tooling experience. Participation is voluntary and when you choose to participate " \
+"your device automatically sends information to Microsoft about how you use Azure IoT "\
+"samples. If you want to change this setting after first time, please delete the "\
+"telemetry.config file and restart the program. "\
+"\n\nSelect y to enable data collection (y/n, default is y). ";
+
+static int enabled = 0;
+
+void initial_telemetry()
+{
+    FILE *file;
+    file = fopen("telemetry.config", "r");
+    if (file != NULL)
+    {
+        char config = (char)fgetc(file);
+        enabled = config == 'y';
+        fclose(file);
+    }
+    else
+    {
+        printf("%s", PROMPT_TEXT);
+        char answer = getchar();
+        file = fopen("telemetry.config", "w");
+        if (answer == 'n')
+        {
+            fputc('n', file);
+            send_telemetry_data_without_sensitive_info("no");
+            enabled = 0;
+        }
+        else
+        {
+            fputc('y', file);
+            send_telemetry_data_without_sensitive_info("yes");
+            enabled = 1;
+        }
+        fclose(file);
+    }
+}
 
 void get_mac_address_hash(char outputBuffer[], int len)
 {
@@ -106,8 +167,52 @@ int substr_count(const char * str, const char * substr)
     return count;
 }
 
+void send_telemetry_data_without_sensitive_info(const char *event)
+{
+    CURL *curl;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, PATH);
+
+        time_t t = time(NULL);
+        char *cur_time = ctime(&t);
+        int tlen = strlen(cur_time) - 1;
+        cur_time[tlen] = 0;
+
+        int str_placeholder_count = substr_count(BODY_TEMPLATE_MIN, "%s");
+        int size = strlen(BODY_TEMPLATE_MIN) + strlen(IKEY) - strlen("%s") * str_placeholder_count
+            + strlen(EVENT) + strlen(event) + tlen + 1;
+
+        char *data = (char *)malloc(size * sizeof(char));
+        if (data != NULL)
+        {
+            snprintf(data, size, BODY_TEMPLATE_MIN, event, cur_time, EVENT, IKEY);
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+            // hide curl output
+            FILE *devnull = fopen("/dev/null", "w+");
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, devnull);
+
+            res = curl_easy_perform(curl);
+            fclose(devnull);
+
+            curl_easy_cleanup(curl);
+
+            free(data);
+        }
+    }
+}
+
 void send_telemetry_data(const char *iotHubName, const char *event, const char *message)
 {
+    if (!enabled)
+    {
+        return;
+    }
+
     CURL *curl;
     CURLcode res;
 
@@ -134,16 +239,16 @@ void send_telemetry_data(const char *iotHubName, const char *event, const char *
 
         exec_command("/usr/bin/lsb_release -r| grep -oP \'\\d\\.\\d\' | tr -d \'\\r\\n\'", os_release);
         exec_command("cat /etc/os-release | grep -oP \'^ID_LIKE=.*$\' | tr -d \'\\r\\n\'", os_platform);
-        int str_placeholder_count = substr_count(BODY_TEMPLATE, "%s");
+        int str_placeholder_count = substr_count(BODY_TEMPLATE_FULL, "%s");
         int platform_str_offset =  strlen(prefix);
-        int size = strlen(BODY_TEMPLATE) + strlen(LANGUAGE) + strlen(DEVICE) + strlen(MCU) + strlen(EVENT)
+        int size = strlen(BODY_TEMPLATE_FULL) + strlen(LANGUAGE) + strlen(DEVICE) + strlen(MCU) + strlen(EVENT)
              + strlen(IKEY) - strlen("%s") * str_placeholder_count + strlen(hash_iothub_name)
              + strlen(hash_mac) + strlen(platform.sysname) + strlen(os_platform + platform_str_offset)
              + strlen(os_release) + strlen(message) + strlen(event) + tlen + 1;
         char *data = (char *)malloc(size * sizeof(char));
         if (data != NULL)
         {
-            snprintf(data, size, BODY_TEMPLATE, LANGUAGE, DEVICE, MCU, message, hash_mac, hash_iothub_name,
+            snprintf(data, size, BODY_TEMPLATE_FULL, LANGUAGE, DEVICE, MCU, message, hash_mac, hash_iothub_name,
                 platform.sysname, os_platform + platform_str_offset, os_release, event, cur_time, EVENT, IKEY);
             curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
             // hide curl output
